9f75709c19d3af77cf536b7d5826968971c52c14
rmp15 <r.parks15@imperial.ac.uk>
2021-12-23 19:57:06 -0500

    added r markdown and new plots

--- data/file_locations/file_locations.R
+++ data/file_locations/file_locations.R
@@ -1,2 +1,3 @@
 # list of important file locations to avoid having to repeat every time loading files
 original_population_data <- "~/data/mortality/US/state/processed/county/"
+new_population_data <- "~/data/cdc_population_monthly_infer/output/population_processed/"
